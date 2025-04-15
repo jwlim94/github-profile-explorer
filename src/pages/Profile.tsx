5de7a5590dc732b004dbf4c6bba97c2ed0ef08c0
Jongwoo Lim <limjongwoo1994@gmail.com>
2025-04-15 17:57:15 -0600

    extract username in Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { username } = useParams<{ username: string }>();
+
+  return (
+    <div className="text-white">
+      <h1>Profile Page</h1>
+      <p>Username: {username}</p>
+    </div>
+  );
 };
 
 export default Profile;
